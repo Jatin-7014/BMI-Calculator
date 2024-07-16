896cced1cd6128024a8cbd6f75bbe0c81cefd9b8
Jatin Kumar <Jatin.Kumar@asl.mum.sst>
2024-07-16 12:36:10 +0530

    Initial Commit

---
+++ Models/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculator.Models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public double Height { get; set; } = 5;
+        public int Weight { get; set; } = 50;
+
+        public Person(int id,string name,int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public Person(int id, string name, int age, int height, int weight) : this(id, name, age)
+        {
+            Height = height;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            string title = "==================Details of individuals:==========================";
+            return $"{title}\n" +
+                $"Id: {Id}\nName: {Name}\nAge: {Age}yrs\nHeight: {Height}ft\nWeight: {Weight}kg\n";
+        }
+
+        public static double CalculateBmiScore(Person person)
+        {
+            double ht = Math.Round((person.Height/3.281),2);
+            double result = person.Weight / (ht * ht);
+            return result;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BmiCalculator.Models;
+
+namespace BmiCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons= new Person[]{new Person(101, "Billu", 21),
+            new Person(102, "Jatin", 22, 6, 50) };
+
+            ShowDetails(persons);
+        }
+
+        static void ShowDetails(Person[] persons)
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person);
+
+                double BmiScore = Math.Round(Person.CalculateBmiScore(person), 3);
+                Console.WriteLine($"BmiScore: {BmiScore}");
+                BodyType(BmiScore);
+            }
+        }
+        static void BodyType(double BmiScore)
+        {
+            if(BmiScore>=18.5 && BmiScore<=24.9)
+                Console.WriteLine($"Your BodyType is normal based on your BMI Score\n");
+            else if(BmiScore>=25)
+                Console.WriteLine($"Your BodyType is over-weight based on your BMI Score\n");
+            else
+                Console.WriteLine($"Your BodyType is under-weight based on your BMI Score\n");
+        }
+    }
+}
